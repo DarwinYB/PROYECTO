de2d9e65c1207f58331efc7c44bdf760bd7cc44a
DarwinYB <darwin&200195>
2020-08-14 11:36:13 -0500

    Cambios necesarios

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 # Create your views here.
 from django.shortcuts import render
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from .forms import CustomUserForm
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
 
@@ -116,3 +119,21 @@ def reporte(request, plantilla="reporte.html"):
 
 def vacaciones(request, plantilla="vacaciones.html"):
     return render(request, plantilla);
+
+
+def registro_docente(request):
+    data = {
+        'form': CustomUserForm()
+    }
+    if request.method =='POST':
+        formulario = CustomUserForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            #autenticar al usuario y redigir al inicio
+            username = formulario.cleaned_data['username']
+            password = formulario.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect(to='home')
+
+    return render(request, 'registro_docente.html', data)
